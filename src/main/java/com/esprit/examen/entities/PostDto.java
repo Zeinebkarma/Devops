da65137b2e7881ea485a32be53ccf639b2099867
oussemabelaid7 <117984014+oussemabelaid7@users.noreply.github.com>
2022-11-14 11:15:50 -0800

    Create PostDto.java

---
+++ src/main/java/com/esprit/examen/entities/PostDto.java
@@ -0,0 +1,10 @@
+package com.esprit.examen.entities;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+	private Long idSecteurActivite;
+	private String codeSecteurActivite;
+	private String libelleSecteurActivite;
+}
